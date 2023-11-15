700245a6cc9550ca415d83368c78617092325b0f
Foued Amiri <fouedamiri10@gmail.com>
2023-11-15 15:59:12 +0100

    connect to mongo database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(() => {console.log('Couldn\'t connect to MongoDB')});
 
 const app = express();
 
@@ -6,6 +14,6 @@ app.listen(3000, (err, res) => {
     if (err) {
         console.log(err);
     } else {
-        console.log('Server is running on port 3000 HOMECD');
+        console.log('Server is running on port 3000');
     }
 });
