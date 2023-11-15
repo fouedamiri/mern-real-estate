2e43b6b04c202ed922193961193f4c6dc867a069
Foued Amiri <fouedamiri10@gmail.com>
2023-11-15 16:26:22 +0100

    Create User model

--- api/index.js
+++ api/index.js
@@ -1,19 +1,23 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-    console.log('Connected to MongoDB');
-}).catch(() => {console.log('Couldn\'t connect to MongoDB')});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch(() => {
+    console.log("Couldn't connect to MongoDB");
+  });
 
 const app = express();
-
 app.listen(3000, (err, res) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Server is running on port 3000');
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Server is running on port 3000");
+  }
 });
